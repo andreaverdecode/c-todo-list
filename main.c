8b5830d86526c86319089c8194be1dab8efd9c2e
andreaverdecode <andreaverde31@gmail.com>
2026-07-03 17:54:35 +0200

    Inserita funzione per aggiungere attivita e funzione conta attivita

--- main.c
+++ main.c
@@ -25,41 +25,78 @@ typedef struct{
 }Activity;
 
 //prototipi di funzione
-void readActivities(FILE * activities, Activity list[], int *num_activities);
+void activitiesCount(FILE *activities, int *num_activities);
+void readActivities(FILE *activities, Activity list[], int *num_activities);
 void printActivities(Activity list[], int *num_activities);
+void addActivities(FILE *activities);
 
 int main(int argc, char *argv[]){  
     FILE * fp = NULL;
     Activity activities[MAX_ACTIVITIES] = {};
-    int dim;
+    int dim, scelta;
 
-    fp = fopen(argv[1], "r"); //apertura del file
-
-    //controllo che il file si apra correttamente
-    if (fp == NULL){
-        printf("Errore nell'apertura del file %s", argv[1]); 
+    //conto le righe all'apertura del file
+    fp = fopen(argv[1], "r");
+    activitiesCount(fp, &dim);
+    if (dim >= MAX_ACTIVITIES){
+        printf("Numero massimo di attivita raggiunto!");
+        return -1;
     }
+    fclose(fp);
+
+    printf("Che azione vuoi compiere:\n");
+    printf("1-Visualizzare la lista\t2-Inserire una nuova attivita: ");
+    scanf("%d", &scelta);
+
+    switch (scelta){
+    case 1:
+        fp = fopen(argv[1], "r"); //apertura del file in modalita lettura
+
+        //controllo che il file si apra correttamente
+        if (fp == NULL){
+            printf("Errore nell'apertura del file %s", argv[1]); 
+        }
+        
+        readActivities(fp, activities, &dim);
+        printActivities(activities, &dim);
+        break;
+
+    case 2:
+        fp = fopen(argv[1], "a"); //apertura del file in modalita aggiunta
+
+        //controllo che il file si apra correttamente
+        if (fp == NULL){
+            printf("Errore nell'apertura del file %s", argv[1]);
+        }
+
+        addActivities(fp);
+        break;
     
-    readActivities(fp, activities, &dim);
-    printActivities(activities, &dim);
+    default:
+        puts("Scelta non valida");
+        break;
+    }
     
     fclose(fp); //chiusura del file
 }
 
-//funzione che permette di leggere e archiviare nella lista il contenuto del file
-void readActivities(FILE * activities, Activity list[], int *num_activities){
-    *num_activities = 0;
 
-    while (feof(activities) == 0 && *num_activities < MAX_ACTIVITIES){
-        fscanf(activities, "%50s", list[*num_activities].activity);
-        fscanf(activities, "%d", &list[*num_activities].is_completed);
-        fscanf(activities, "%2d", &list[*num_activities].creation_date.day);
-        fscanf(activities, "%*c%2d", &list[*num_activities].creation_date.month);
-        fscanf(activities, "%*c%4d", &list[*num_activities].creation_date.year);
+void activitiesCount(FILE *activities, int *num_activities){
+    *num_activities = 0;
+    char buffer[100];
+    while (fgets(buffer, 100, activities) != NULL){
         (*num_activities)++;
-        if (fgetc(activities) == EOF){
-            break;
-        }
+    }
+}
+
+//funzione che permette di leggere e archiviare nella lista il contenuto del file
+void readActivities(FILE *activities, Activity list[], int *num_activities){
+    for(int i = 0; i < *num_activities; i++){
+        fscanf(activities, "%50s", list[i].activity);
+        fscanf(activities, "%d", &list[i].is_completed);
+        fscanf(activities, "%2d", &list[i].creation_date.day);
+        fscanf(activities, "%*c%2d", &list[i].creation_date.month);
+        fscanf(activities, "%*c%4d", &list[i].creation_date.year);
     }    
 }
 
@@ -70,7 +107,26 @@ void printActivities(Activity list[], int *num_activities){
         printf("Numero %d\n", i + 1);
         printf("\tAttivita': %s\n", list[i].activity);
         printf("\tStato: %d\n", list[i].is_completed);
-        printf("\tData di creazione: %d/%d/%d\n", list[i].creation_date.day, list[i].creation_date.month, list[i].creation_date.year);\
+        printf("\tData di creazione: %d/%d/%d\n", list[i].creation_date.day, list[i].creation_date.month, list[i].creation_date.year);
     }
 }
+
+void addActivities(FILE *activities){
+    char name[MAX_LENGHT];
+    int state, d_date, m_date, y_date;
+
+    printf("Inserisci la nuova attivita': ");
+    scanf("%50s", name);
+    printf("Inserisci la data di creazione\n");
+    printf("Giorno: ");
+    scanf("%d", &d_date);
+    printf("Mese: ");
+    scanf("%d", &m_date);
+    printf("Anno: ");
+    scanf("%d", &y_date);
+
+    fprintf(activities, "\n%s %d %d/%d/%d", name, 0, d_date, m_date, y_date);
+}
+
+
 
